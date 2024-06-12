96d5be1167e175e27ddbc7a6389abdc74f0941c2
Rasmus <Frandsen123443@gmail.com>
2024-06-12 10:54:28 +0200

    code that doesn't work

--- backend/PizzaStore/Program.cs
+++ backend/PizzaStore/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+builder.Services.AddEndpointsApiExplorer();
+if (app.Environment.IsDevelopment())
+{
+   builder.Services.AddSwaggerGen(c =>
+     {
+       c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Description = "Keep track of your tasks", Version = "v1" });
+     });
+   app.UseSwagger();
+   app.UseSwaggerUI(c =>
+    {
+      c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+    });
+} // end of if (app.Environment.IsDevelopment()) block
 app.Run();
